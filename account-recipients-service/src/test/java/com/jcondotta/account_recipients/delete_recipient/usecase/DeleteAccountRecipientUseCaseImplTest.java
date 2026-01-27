b4bd3cca415ff176d3f6b42dfe5e56a61ea1d191
jcondotta <jefferson.condotta@gmail.com>
2026-01-27 15:14:39 +0100

    Remove unused imports and redundant code across modules
    Eliminated unused imports, methods, and variables to improve code clarity and maintainability. No functional changes were introduced, ensuring that all existing behavior remains unaffected.

--- account-recipients-service/src/test/java/com/jcondotta/account_recipients/delete_recipient/usecase/DeleteAccountRecipientUseCaseImplTest.java
+++ account-recipients-service/src/test/java/com/jcondotta/account_recipients/delete_recipient/usecase/DeleteAccountRecipientUseCaseImplTest.java
@@ -1,19 +1,37 @@
 package com.jcondotta.account_recipients.delete_recipient.usecase;
 
+import com.jcondotta.account_recipients.ClockTestFactory;
+import com.jcondotta.account_recipients.application.events.mapper.RecipientDeletedEventMapper;
+import com.jcondotta.account_recipients.application.ports.output.cache.AccountRecipientsRootCacheKey;
 import com.jcondotta.account_recipients.application.ports.output.cache.CacheStore;
+import com.jcondotta.account_recipients.application.ports.output.messaging.RecipientDeletedEventPublisher;
 import com.jcondotta.account_recipients.application.ports.output.repository.delete_recipient.DeleteAccountRecipientRepository;
 import com.jcondotta.account_recipients.application.ports.output.repository.get_recipient.GetAccountRecipientRepository;
+import com.jcondotta.account_recipients.application.usecase.delete_recipient.model.DeleteAccountRecipientCommand;
 import com.jcondotta.account_recipients.application.usecase.get_recipients.model.result.GetAccountRecipientsResult;
+import com.jcondotta.account_recipients.application.usecase.shared.value_objects.IdempotencyKey;
+import com.jcondotta.account_recipients.domain.recipient.entity.AccountRecipient;
+import com.jcondotta.account_recipients.domain.recipient.events.RecipientDeletedEvent;
+import com.jcondotta.account_recipients.domain.recipient.value_objects.Iban;
 import com.jcondotta.account_recipients.domain.recipient.value_objects.RecipientId;
+import com.jcondotta.account_recipients.domain.recipient.value_objects.RecipientName;
 import com.jcondotta.account_recipients.domain.shared.value_objects.BankAccountId;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.util.Optional;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteAccountRecipientUseCaseImplTest {
@@ -22,7 +40,17 @@ class DeleteAccountRecipientUseCaseImplTest {
   private static final UUID ACCOUNT_RECIPIENT_UUID = UUID.randomUUID();
 
   private static final BankAccountId BANK_ACCOUNT_ID = BankAccountId.of(BANK_ACCOUNT_UUID);
-  private static final RecipientId ACCOUNT_RECIPIENT_ID = RecipientId.of(ACCOUNT_RECIPIENT_UUID);
+  private static final RecipientId RECIPIENT_ID = RecipientId.of(ACCOUNT_RECIPIENT_UUID);
+  private static final RecipientName RECIPIENT_NAME_JEFFERSON = RecipientName.of("Jefferson Condotta");
+  private static final Iban IBAN = Iban.of("GB82WEST12345698765432");
+
+  private static final IdempotencyKey IDEMPOTENCY_KEY = IdempotencyKey.newKey();
+
+  private static final Clock FIXED_CLOCK = ClockTestFactory.TEST_CLOCK_FIXED;
+  private static final ZonedDateTime CREATED_AT = ZonedDateTime.now(FIXED_CLOCK);
+
+  @Mock
+  private AccountRecipient accountRecipientMock;
 
   @Mock
   private GetAccountRecipientRepository getAccountRecipientRepository;
@@ -31,43 +59,73 @@ class DeleteAccountRecipientUseCaseImplTest {
   private DeleteAccountRecipientRepository deleteAccountRecipientRepository;
 
   @Mock
-  private CacheStore<GetAccountRecipientsResult> cacheStoreMock;
+  private CacheStore<GetAccountRecipientsResult> cacheStore;
+
+  @Mock
+  private RecipientDeletedEventPublisher deletedEventPublisher;
+
+  @Mock
+  private RecipientDeletedEventMapper eventMapper;
 
   @Captor
-  private ArgumentCaptor<BankAccountId> bankAccountIdCaptor;
-
-  @Captor
-  private ArgumentCaptor<RecipientId> recipientIdCaptor;
+  private ArgumentCaptor<AccountRecipient> accountRecipientCaptor;
 
   private DeleteAccountRecipientUseCaseImpl useCase;
 
   @BeforeEach
   void setUp() {
-//    useCase = new DeleteAccountRecipientUseCaseImpl(getAccountRecipientRepository, deleteAccountRecipientRepository, cacheStoreMock);
+    useCase =
+        new DeleteAccountRecipientUseCaseImpl(
+            getAccountRecipientRepository,
+            deleteAccountRecipientRepository,
+            cacheStore,
+            deletedEventPublisher,
+            eventMapper,
+            FIXED_CLOCK);
   }
 
-//  @Test
-//  void shouldDeleteRecipient_whenCommandIsValid() {
-//    var command = DeleteAccountRecipientCommand.of(BANK_ACCOUNT_ID, ACCOUNT_RECIPIENT_ID);
-//
-//    useCase.execute(command);
-//
-//    verify(deleteAccountRecipientRepository).delete(bankAccountIdCaptor.capture(), recipientIdCaptor.capture());
-//    assertThat(bankAccountIdCaptor.getValue()).isEqualTo(BANK_ACCOUNT_ID);
-//    assertThat(recipientIdCaptor.getValue()).isEqualTo(ACCOUNT_RECIPIENT_ID);
-//
-//    var cacheKey = AccountRecipientsRootCacheKey.of(BANK_ACCOUNT_ID);
-//    verify(cacheStoreMock).evictKeysByPrefix(cacheKey.value());
-//
-//    verifyNoMoreInteractions(deleteAccountRecipientRepository, cacheStoreMock);
-//  }
+  @Test
+  void shouldDeleteRecipientAndPublishEvent_whenCommandIsValid() {
+    var command = DeleteAccountRecipientCommand.of(BANK_ACCOUNT_ID, RECIPIENT_ID);
 
-//  @Test
-//  void shouldNotInteractWithRepository_whenCommandIsNull() {
-//    assertThatThrownBy(() -> useCase.execute(null))
-//        .isInstanceOf(NullPointerException.class)
-//        .hasMessage("Command must not be null");
-//
-//    verifyNoInteractions(deleteAccountRecipientRepository, cacheStoreMock);
-//  }
+    when(getAccountRecipientRepository.getAccountRecipient(BANK_ACCOUNT_ID, RECIPIENT_ID))
+        .thenReturn(Optional.of(accountRecipientMock));
+
+    when(accountRecipientMock.getBankAccountId()).thenReturn(BANK_ACCOUNT_ID);
+
+    var event = new RecipientDeletedEvent(RECIPIENT_ID, BANK_ACCOUNT_ID, FIXED_CLOCK.instant(), FIXED_CLOCK.getZone());
+    when(eventMapper.fromAccountRecipient(accountRecipientMock)).thenReturn(event);
+
+    useCase.execute(command, IDEMPOTENCY_KEY);
+
+    verify(accountRecipientMock).delete(FIXED_CLOCK);
+
+    verify(deletedEventPublisher).send(event, IDEMPOTENCY_KEY);
+
+    verify(deleteAccountRecipientRepository)
+        .delete(accountRecipientCaptor.capture());
+
+    assertThat(accountRecipientCaptor.getValue()).isEqualTo(accountRecipientMock);
+
+    var cacheKey = AccountRecipientsRootCacheKey.of(BANK_ACCOUNT_ID);
+    verify(cacheStore).evictKeysByPrefix(cacheKey.value());
+
+    verifyNoMoreInteractions(
+        deleteAccountRecipientRepository,
+        deletedEventPublisher,
+        cacheStore);
+  }
+
+  @Test
+  void shouldThrowException_whenCommandIsNull() {
+    assertThatThrownBy(() -> useCase.execute(null, IDEMPOTENCY_KEY))
+        .isInstanceOf(NullPointerException.class)
+        .hasMessage("Command must not be null");
+
+    verifyNoInteractions(
+        getAccountRecipientRepository,
+        deleteAccountRecipientRepository,
+        deletedEventPublisher,
+        cacheStore);
+  }
 }
